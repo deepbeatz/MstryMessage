9f0246f11784ee6a7ea4c41040fce44eaf940063
Rajdeep Chowdhury <rajdeepchowdhury318@gmail.com>
2024-11-23 21:16:11 +0530

    3rd commit

--- src/app/api/send-message/route.ts
+++ src/app/api/send-message/route.ts
@@ -4,5 +4,6 @@ import UserModel from "@/model/User";
 
 export async function POST(request:Request){
     await dbConnect()
+    //note that message can be sent by anyone who has the link, the person doesnt have to be a user of this website, doesnt have to be logged in
     
 }
